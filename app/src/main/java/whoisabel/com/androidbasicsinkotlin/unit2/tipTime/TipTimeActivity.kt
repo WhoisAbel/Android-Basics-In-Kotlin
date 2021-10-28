af226e2b41f0fd4e731a12611d3f61759704c591
abolfazlkalamati <Jq=.8b#pD94YwkZ>
2021-10-28 17:05:29 +0330

    Calculate the tip (#7)

--- app/src/main/java/whoisabel/com/androidbasicsinkotlin/unit2/tipTime/TipTimeActivity.kt
+++ app/src/main/java/whoisabel/com/androidbasicsinkotlin/unit2/tipTime/TipTimeActivity.kt
@@ -3,10 +3,47 @@ package whoisabel.com.androidbasicsinkotlin.unit2.tipTime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import whoisabel.com.androidbasicsinkotlin.R
+import whoisabel.com.androidbasicsinkotlin.databinding.ActivityTipTimeBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class TipTimeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTipTimeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip_time)
+        binding = ActivityTipTimeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnCalculateButton.setOnClickListener { calculateTip() }
+
     }
+
+
+    private fun calculateTip() {
+
+        val stringInTextField = binding.etCostOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tvTipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.rgTipOptions.checkedRadioButtonId) {
+            R.id.rb_option_twenty_percent -> 0.20
+            R.id.rb_option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        val roundUp = binding.swRoundUpSwitch.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tvTipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
 }
