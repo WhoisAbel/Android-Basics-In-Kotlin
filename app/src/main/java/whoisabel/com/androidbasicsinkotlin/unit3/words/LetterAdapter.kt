58c350b30381b7ba6edf53cdd07639917ce812d0
abolfazlkalamati <Jq=.8b#pD94YwkZ>
2021-11-07 14:50:22 +0330

    Activities and intent (#36)

---
+++ app/src/main/java/whoisabel/com/androidbasicsinkotlin/unit3/words/LetterAdapter.kt
@@ -0,0 +1,82 @@
+package whoisabel.com.androidbasicsinkotlin.unit3.words
+
+import android.content.Intent
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import whoisabel.com.androidbasicsinkotlin.R
+import whoisabel.com.androidbasicsinkotlin.unit3.words.DetailActivity.Companion.LETTER
+
+
+/**
+ * Adapter for the [RecyclerView] in [LetterActivity].
+ */
+class LetterAdapter : RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
+
+    // Generates a [CharRange] from 'A' to 'Z' and converts it to a list
+    private val list = ('A').rangeTo('Z').toList()
+
+    /**
+     * Provides a reference for the views needed to display items in your list.
+     */
+    class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * Creates new views with R.layout.item_view as its template
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_words, parent, false)
+        // Setup custom accessibility delegate to set the text read
+        layout.accessibilityDelegate = Accessibility
+        return LetterViewHolder(layout)
+    }
+
+    /**
+     * Replaces the content of an existing view with new data
+     */
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        val item = list[position]
+        holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val intent = Intent(holder.view.context, DetailActivity::class.java)
+            intent.putExtra(LETTER, holder.button.text.toString())
+            holder.view.context.startActivity(intent)
+        }
+    }
+
+    // Setup custom accessibility delegate to set the text read with
+    // an accessibility service
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host?.context?.getString(R.string.look_up_words)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+}
